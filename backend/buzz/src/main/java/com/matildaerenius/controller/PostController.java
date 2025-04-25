6f17a5c92a21a995d149e775295ce31e26530914
Matilda Erenius <matildaerenius@hotmail.com>
2025-04-25 11:49:48 +0200

    feat: add PostController & ApiResponse

---
+++ backend/buzz/src/main/java/com/matildaerenius/controller/PostController.java
@@ -0,0 +1,61 @@
+package com.matildaerenius.controller;
+
+import com.matildaerenius.models.Post;
+import com.matildaerenius.response.ApiResponse;
+import com.matildaerenius.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    PostService postService;
+
+    @PostMapping("/posts/user/{userId}")
+    public ResponseEntity<Post> createPost(@RequestBody Post post, @PathVariable int userId) throws Exception {
+        Post createdPost = postService.createNewPost(post, userId);
+        return new ResponseEntity<>(createdPost, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/posts/{postId}/user/{userId}")
+    public ResponseEntity<ApiResponse> deletePost(@PathVariable int postId, @PathVariable int userId) throws Exception {
+        String message = postService.deletePost(postId, userId);
+        ApiResponse res = new ApiResponse(message,true);
+        return new ResponseEntity<ApiResponse>(res, HttpStatus.OK);
+    }
+
+    @GetMapping("/posts/{postId}")
+    public ResponseEntity<Post> findPostByIdHandler(@PathVariable int postId) throws Exception {
+        Post post = postService.findPostById(postId);
+        return new ResponseEntity<>(post, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/posts/user/{userId}")
+    public ResponseEntity<List<Post>> findUsersPosts(@PathVariable int userId) throws Exception {
+        List<Post> posts = postService.findPostByUserId(userId);
+        return new ResponseEntity<List<Post>>(posts, HttpStatus.OK);
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> findAllPosts() throws Exception {
+        List<Post> posts = postService.findAllPosts();
+        return new ResponseEntity<List<Post>>(posts, HttpStatus.OK);
+    }
+
+    @PutMapping("/posts/{postId}/user/{userId}")
+    public ResponseEntity<Post> savePostHandler(@PathVariable int postId, @PathVariable int userId) throws Exception {
+        Post post = postService.savedPost(postId, userId);
+        return new ResponseEntity<>(post, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/posts/like/{postId}/user/{userId}")
+    public ResponseEntity<Post> likePostHandler(@PathVariable int postId, @PathVariable int userId) throws Exception {
+        Post post = postService.likePost(postId, userId);
+        return new ResponseEntity<>(post, HttpStatus.ACCEPTED);
+    }
+}
