dc65f8c59c4f422bf9b11cb04e5c1066409be1ab
Matilda Erenius <matildaerenius@hotmail.com>
2025-04-28 13:35:49 +0200

    feat: add Reels model and lombok dependency

---
+++ backend/buzz/src/main/java/com/matildaerenius/models/Reels.java
@@ -0,0 +1,28 @@
+package com.matildaerenius.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reels {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String title;
+
+    private String video;
+
+    @ManyToOne
+    private User user;
+
+}
