b1244176ed836b27c59dacd2d6460d8355f074cf
Matilda Erenius <matildaerenius@hotmail.com>
2025-04-28 09:51:17 +0200

    feat: add Comment model

---
+++ backend/buzz/src/main/java/com/matildaerenius/models/Comment.java
@@ -0,0 +1,72 @@
+package com.matildaerenius.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String content;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToMany
+    private List<User> liked = new ArrayList<User>();
+
+    private LocalDateTime createdAt;
+
+    public Comment() {}
+
+    public Comment(String content, User user) {
+        this.content = content;
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<User> getLiked() {
+        return liked;
+    }
+
+    public void setLiked(List<User> liked) {
+        this.liked = liked;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
