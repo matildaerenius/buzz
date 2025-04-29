2ef42d05caabc6552e1468cebbfa92f78246b4bb
Matilda Erenius <matildaerenius@hotmail.com>
2025-04-29 17:13:21 +0200

    feat: add StoryService implementation and ServiceController

---
+++ backend/buzz/src/main/java/com/matildaerenius/controller/StoryController.java
@@ -0,0 +1,38 @@
+package com.matildaerenius.controller;
+
+import com.matildaerenius.models.Story;
+import com.matildaerenius.models.User;
+import com.matildaerenius.service.StoryService;
+import com.matildaerenius.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class StoryController {
+
+    @Autowired
+    private StoryService storyService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/api/story")
+    public Story createStory(@RequestBody Story story, @RequestHeader("Authorization")String jwt) throws Exception {
+
+        User reqUser = userService.findUserByJwt(jwt);
+        Story createdStory = storyService.createStory(story, reqUser);
+
+        return createdStory;
+    }
+
+    @GetMapping("/api/story/{userId}")
+    public List<Story> findUserStory(@PathVariable Integer userId, @RequestHeader("Authorization")String jwt) throws Exception {
+
+        User reqUser = userService.findUserByJwt(jwt);
+        List<Story> stories = storyService.findStoryByUserId(userId);
+
+        return stories;
+    }
+}
--- backend/buzz/src/main/java/com/matildaerenius/service/StoryService.java
+++ backend/buzz/src/main/java/com/matildaerenius/service/StoryService.java
@@ -1,14 +1,15 @@
 package com.matildaerenius.service;
 
 import com.matildaerenius.models.Story;
+import com.matildaerenius.models.User;
 
 import java.util.List;
 
 public interface StoryService {
 
-    public Story createStory (Story story, Integer userId);
+    public Story createStory (Story story, User user);
 
-    public List<Story> findStoryByUserId (Integer userId);
+    public List<Story> findStoryByUserId (Integer userId) throws Exception;
 
 
 
