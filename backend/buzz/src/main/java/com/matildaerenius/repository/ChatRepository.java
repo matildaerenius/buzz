c961b9cc93489d85144af95b27aeb2f444119c66
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-01 12:51:28 +0200

    feat: add ChatRepository interface and implementation

--- backend/buzz/src/main/java/com/matildaerenius/repository/ChatRepository.java
+++ backend/buzz/src/main/java/com/matildaerenius/repository/ChatRepository.java
@@ -1,11 +1,17 @@
 package com.matildaerenius.repository;
 
 import com.matildaerenius.models.Chat;
+import com.matildaerenius.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Integer> {
 
     public List<Chat> findByUsersId(Integer userId);
+
+    @Query("select c from Chat c Where :user Member of c.users And :reqUser Member of c.users")
+    public Chat findChatByUsersId(@Param("users") User user, @Param("reqUser") User reqUser);
 }
--- backend/buzz/src/main/java/com/matildaerenius/service/ChatService.java
+++ backend/buzz/src/main/java/com/matildaerenius/service/ChatService.java
@@ -9,7 +9,7 @@ public interface ChatService {
 
     public Chat createChat(User reqUser, User user2);
 
-    public Chat findChatById(Integer chatId);
+    public Chat findChatById(Integer chatId) throws Exception;
 
     public List<Chat> findUsersChat(Integer userId);
 }
