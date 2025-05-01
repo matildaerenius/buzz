c0335501ea210f9d4e9960fd1016d23d63745c9c
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-01 13:38:34 +0200

    feat(chat): implement ChatController and CreateChatRequest DTO

---
+++ backend/buzz/src/main/java/com/matildaerenius/controller/ChatController.java
@@ -0,0 +1,40 @@
+package com.matildaerenius.controller;
+
+import com.matildaerenius.models.Chat;
+import com.matildaerenius.models.User;
+import com.matildaerenius.request.CreateChatRequest;
+import com.matildaerenius.service.ChatService;
+import com.matildaerenius.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ChatController {
+
+    @Autowired
+    private ChatService chatService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/api/chats")
+    public Chat createChat(@RequestHeader("Authorization") String jwt, @RequestBody CreateChatRequest req) throws Exception {
+        User reqUser = userService.findUserByJwt(jwt);
+        User user2 = userService.findUserById(req.getUserId());
+        Chat chat = chatService.createChat(reqUser, user2);
+
+        return chat;
+    }
+
+    @GetMapping("/api/chats")
+    public List<Chat> findUsersChat(@RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.findUserByJwt(jwt);
+
+        List<Chat> chats = chatService.findUsersChat(user.getId());
+
+        return chats;
+    }
+}
---
+++ backend/buzz/src/main/java/com/matildaerenius/request/CreateChatRequest.java
@@ -0,0 +1,14 @@
+package com.matildaerenius.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateChatRequest {
+
+    private Integer userId;
+
+}
