c67105c5cdf12cc4f9fdeeeabb6dad08b868162e
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-01 13:37:35 +0200

    refactor: replace manual getters/setters and constructors with Lombok annotations

--- backend/buzz/src/main/java/com/matildaerenius/response/AuthResponse.java
+++ backend/buzz/src/main/java/com/matildaerenius/response/AuthResponse.java
@@ -1,30 +1,16 @@
 package com.matildaerenius.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
 
     private String token;
     private String message;
 
-    AuthResponse() {}
-
-    public AuthResponse(String token, String message) {
-        this.token = token;
-        this.message = message;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
