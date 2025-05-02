ec3d26f2dbdf92ee9573130e0a771974c6505d31
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-02 12:34:53 +0200

    feat: add global exception handling with ErrorDetails and GlobalException

---
+++ backend/buzz/src/main/java/com/matildaerenius/exception/ErrorDetails.java
@@ -0,0 +1,19 @@
+package com.matildaerenius.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+    private String message;
+    private String error;
+    private LocalDateTime timestamp;
+
+}
---
+++ backend/buzz/src/main/java/com/matildaerenius/exception/GlobalException.java
@@ -0,0 +1,23 @@
+package com.matildaerenius.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> otherExceptionHandler(Exception ue, WebRequest req) {
+
+        ErrorDetails errorDetails = new ErrorDetails(ue.getMessage(),
+                req.getDescription(false),
+                LocalDateTime.now());
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+}
