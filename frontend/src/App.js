7fffddcd5b32bf5ec62d5541b19f082a135116ed
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-03 22:05:32 +0200

    feat: set up React Router with HomePage and Message routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Authentication from './pages/Authentication/Authentication';
+import Message from './pages/Message/Message';
+import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="">
 
-      <Authentication/>
-    
+      <Routes>
+        <Route path='/' element={<Authentication />} />
+        <Route path='/home' element={<HomePage/>} />
+        <Route path='/message' element={<Message/>} />
+      </Routes>
+
+
+
     </div>
   );
 }
