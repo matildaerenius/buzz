ef2f35b861dd0548252c3efd2ef2164478327e48
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-04 16:17:02 +0200

    feat(sidebar): complete Sidebar with navigation menu, avatar and options menu

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { navigationMenu } from "./SidebarNavigation";
+import { Avatar, Button, Card, Divider, Menu, MenuItem } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const Sidebar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <div className="card h-screen flex flex-col justify-between py-5">
+    <Card className="card h-screen flex flex-col justify-between py-5">
       <div className="space-y-8 pl-5">
         <div className="">
           <span className="logo font-bold text-xl">Buzz</span>
@@ -17,7 +27,42 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
-    </div>
+      <div>
+        <Divider />
+        <div className="pl-5 flex items-center justify-between pt-5">
+          <div className="flex items-center space-x-3">
+            <Avatar src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" />
+
+            <div>
+              <p className="font-bold">Jane Doe</p>
+              <p className="opacity-70">@username</p>
+            </div>
+          </div>
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <MoreVertIcon />
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+        </div>
+      </div>
+    </Card>
   );
 };
 
