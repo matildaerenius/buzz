5d68772707fa177a4faa6f11649ec576d44de216
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-04 15:39:42 +0200

    fix(sidebar): add missing flex class and vertical padding to navigation items

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -3,14 +3,14 @@ import { navigationMenu } from "./SidebarNavigation";
 
 const Sidebar = () => {
   return (
-    <div className="card h-screen flex-col justify-between py-5">
+    <div className="card h-screen flex flex-col justify-between py-5">
       <div className="space-y-8 pl-5">
         <div className="">
           <span className="logo font-bold text-xl">Buzz</span>
         </div>
         <div className="space-y-8">
           {navigationMenu.map((item) => (
-            <div className=" cursor-pointerflex space-x-3 items-center">
+            <div className="flex items-center space-x-3 py-3 cursor-pointer">
               {item.icon}
               <p className="text-xl">{item.title}</p>
             </div>
