2af75be206e587925e2e1e053626f62690e72d64
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-04 16:43:55 +0200

    feat(stories): add StoryCircle component and integrate into MiddlePart for user stories

--- frontend/src/components/MiddlePart/MiddlePart.jsx
+++ frontend/src/components/MiddlePart/MiddlePart.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Avatar } from "@mui/material";
+import React from "react";
+import AddIcon from "@mui/icons-material/Add";
+import StoryCircle from "./StoryCircle";
 
+const story=[11,1,1,1,1]
 const MiddlePart = () => {
   return (
-    <div>MiddlePart</div>
-  )
-}
+    <div className="px-20">
+      <div className="flex items-center p-5 rounded-b-md">
+        <div className="flex flex-col items-center mr-4 cursor-pointer">
+          <Avatar
+            sx={{ width: "5rem", height: "5rem" }}
+            // src="https://cdn.pixabay.com/photo/2020/05/24/05/36/woman-5212479_1280.png"
+          >
+            <AddIcon sx={{ fontSize: "3rem" }} />
+          </Avatar>
+          <p>New</p>
+        </div>
+{story.map((item)=><StoryCircle/>)}
+      </div>
+    </div>
+  );
+};
 
-export default MiddlePart
+export default MiddlePart;
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -41,3 +41,4 @@ export const HomePage = () => {
     </div>
   );
 };
+export default HomePage;
